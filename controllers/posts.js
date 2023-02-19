f90cf4006b4b8bec28a1531262cc7df4feb13934
Thuong_e <dothuong2k1@gmail.com>
2023-02-19 17:09:52 +0700

    new file:   .env new file:   .gitignore new file:   Procfile new file:   README.md new file:   controllers/posts.js new file:   index.js new file:   models/PostModel.js new file:   package-lock.json new file:   package.json new file:   routers/posts.js new file:   vercel.json

---
+++ controllers/posts.js
@@ -0,0 +1,52 @@
+import { PostModel } from "../models/PostModel.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const newPost = req.body;
+
+    const post = new PostModel(newPost);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const updatePost = req.body;
+
+    const post = await PostModel.findOneAndUpdate({ _id: updatePost._id }, updatePost, {
+      new: true,
+    });
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const deletePost = req.body;
+    const post = await PostModel.findOneAndDelete(
+      { _id: deletePost._id },
+      {
+        new: false,
+      }
+    );
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
